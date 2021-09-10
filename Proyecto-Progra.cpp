f7d4eadb36651ddc65d4647e053e67e0b03c5a77
Pollogamerz <85629754+Pollogamerz@users.noreply.github.com>
2021-09-10 00:14:45 -0500

    Upload

---
+++ Proyecto-Progra.cpp
@@ -0,0 +1,13 @@
+#include "game.hh"
+#include "Constants.hh"
+#include "rectangle.hh"
+
+rectangle* Rectangle0{new rectangle(50, 30, 80, 100, sf::Color::Yellow)};
+rectangle* Rectangle1{new rectangle(50, 30, 150, 100, sf::Color::Yellow)};
+rectangle* Rectangle0{new rectangle(25, 30, 75, 125, sf::Color::Yellow)};
+rectangle* Rectangle1{new rectangle(50, 30, 150, 100, sf::Color::Yellow)};
+rectangle* Rectangle0{new rectangle(50, 30, 80, 150, sf::Color::Yellow)};
+rectangle* Rectangle1{new rectangle(25, 30, 150, 100, sf::Color::Yellow)};
+rectangle* Rectangle0{new rectangle(50, 30, 75, 200, sf::Color::Yellow)};
+rectangle* Rectangle1{new rectangle(50, 30, 150, 100, sf::Color::Yellow)};
+rectangle* Rectangle0{new rectangle(25, 30, 1200, 175, sf::Color::Yellow)};
